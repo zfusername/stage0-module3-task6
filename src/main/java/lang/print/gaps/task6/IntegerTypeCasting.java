958fc2a029b2fbc53ffcb6ffe98232cd43be8892
zfusername <farrukh_ziyaev@epam.com>
2023-07-20 12:27:45 +0500

    change

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long first = 1000;
+        int second = (int)first;
+        System.out.println(first + "," + second);
     }
 }
